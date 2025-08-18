8fa0716f5fc21dbc3a0fdf167bf3903a7b04446a
pranay <pranay2632@gmail.com>
2025-08-18 22:04:59 +0530

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3006;
+const port = 4000;
 app.use(cors())
 // Middleware
 app.use(bodyParser.json());
